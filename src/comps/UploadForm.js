41fdf8dc0a6c9a145cd9c631aa6f1057f5203373
Nitin Ranganath <nitinranganath@gmail.com>
2020-10-15 22:15:58 +0530

    Build and style progress bar component

--- src/comps/UploadForm.js
+++ src/comps/UploadForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import ProgressBar from "./ProgressBar";
 
 const UploadForm = () => {
   const [file, setFile] = useState(null);
@@ -18,10 +19,14 @@ const UploadForm = () => {
 
   return (
     <form>
-      <input type="file" onChange={onFileSelect} />
+      <label>
+        <input type="file" onChange={onFileSelect} />
+        <span>+</span>
+      </label>
       <div className="output">
         {error && <div className="error">{error}</div>}
         {file && <div>{file.name}</div>}
+        {file && <ProgressBar file={file} setFile={setFile} />}
       </div>
     </form>
   );
